ceccff00c5f6ddd65bfbe84fa332097adfd7a90c
braheezy <michael.c.braha@gmail.com>
2023-07-29 17:23:26 -0700

    Simplify game start

--- cmd/ms-pacman/main.go
+++ cmd/ms-pacman/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/braheezy/ms-pacman/internal/assets"
 	"github.com/braheezy/ms-pacman/internal/game"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -13,12 +12,9 @@ func main() {
 	ebiten.SetWindowSize(game.Config.Width, game.Config.Height)
 	ebiten.SetWindowTitle("Ms. Pacman")
 
-	img, err := assets.CreateLevelImage("level1")
-	if err != nil {
-		panic(err)
-	}
+	g := game.New()
 
-	if err := ebiten.RunGame(&game.Game{CurrentLevelImage: img}); err != nil {
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
