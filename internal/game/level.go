28cec8bb272ba3a1099794e0f390f5a9240bc9a6
braheezy <michael.c.braha@gmail.com>
2023-09-04 15:41:42 -0700

    Refactor movement into Player entity
    This also cleaned up a couple behaviors: changing direction into a wall does nothing; corner turns can be commanded well before the turn

--- internal/game/level.go
+++ internal/game/level.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"image"
 	"log"
 
 	"github.com/braheezy/ms-pacman/internal/assets"
@@ -19,26 +18,35 @@ var levels = []Level{
 	{
 		name: "Level 1",
 		player: Player{
-			tileX:     13,
-			tileY:     23,
-			pixelX:    13 * assets.TileSize,
-			pixelY:    23*assets.TileSize - (assets.TileSize / 2),
-			image:     assets.LoadSprite("mspac_Lnorm"),
-			direction: Left,
-			moveSpeed: playerSpeedForLevel(1),
+			currentTilePos: TilePos{
+				X: 13,
+				Y: 23,
+			},
+			currentPixelPos: PixelPos{
+				X: 13 * assets.TileSize,
+				Y: 23*assets.TileSize - (assets.TileSize / 2),
+			},
+			nextPixelPos: PixelPos{
+				X: 13 * assets.TileSize,
+				Y: 23*assets.TileSize - (assets.TileSize / 2),
+			},
+			image:              assets.LoadSprite("mspac_Lnorm"),
+			currentDirection:   Left,
+			requestedDirection: Left,
+			moveSpeed:          playerSpeedForLevel(1),
 		},
 	},
 }
 
-func playerSpeedForLevel(level int) float64 {
+func playerSpeedForLevel(level int) float32 {
 	if level == 1 {
-		return 0.8 * Config.MaxMoveSpeed / float64(ebiten.TPS())
+		return 0.8 * Config.MaxMoveSpeed / float32(ebiten.TPS())
 	} else if level >= 2 && level <= 4 {
-		return 0.9 * Config.MaxMoveSpeed / float64(ebiten.TPS())
+		return 0.9 * Config.MaxMoveSpeed / float32(ebiten.TPS())
 	} else if level >= 5 && level <= 20 {
-		return Config.MaxMoveSpeed / float64(ebiten.TPS())
+		return Config.MaxMoveSpeed / float32(ebiten.TPS())
 	} else {
-		return 0.9 * Config.MaxMoveSpeed / float64(ebiten.TPS())
+		return 0.9 * Config.MaxMoveSpeed / float32(ebiten.TPS())
 	}
 }
 
@@ -54,38 +62,6 @@ func newDefaultLevel() *Level {
 	return &firstLevel
 }
 
-func (l *Level) MovePlayer() {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		l.player.direction = Right
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		l.player.direction = Left
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
-		l.player.direction = Up
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
-		l.player.direction = Down
-	}
-
-	nextX, nextY := l.player.getNextPixelCoord()
-	nextTileX, nextTileY := l.player.getNextTileCoord()
-	nextTile := l.grid[nextTileY][nextTileX]
-
-	if nextTile.Type == assets.TileTypeWall {
-		boundingBox := image.Rect(int(nextX), int(nextY), int(nextX+assets.SpriteSize), int(nextY+assets.SpriteSize))
-
-		for y := boundingBox.Min.Y; y < boundingBox.Max.Y; y++ {
-			for x := boundingBox.Min.X; x < boundingBox.Max.X; x++ {
-				r, g, b, _ := l.image.At(x, y).RGBA()
-				// Magic numbers avoid pellet color
-				if (0 < r && r < 57054) || (0 < g && g < 57054) || (0 < b && b < 65535) {
-					return
-				}
-			}
-		}
-	}
-	l.player.pixelX, l.player.pixelY = nextX, nextY
-	l.player.updateTileLocation()
-}
-
 func (l *Level) Update() {
-	l.MovePlayer()
+	l.player.Update(&l.grid, l.image)
 }
--- internal/game/player.go
+++ internal/game/player.go
@@ -1,6 +1,8 @@
 package game
 
 import (
+	"image"
+
 	"github.com/braheezy/ms-pacman/internal/assets"
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -8,105 +10,119 @@ import (
 type Player struct {
 	// The current sprite to show
 	image *ebiten.Image
-	// The current movement direction
-	direction Direction
+	// The current movement currentDirection
+	currentDirection Direction
+	// The input the user has requested
+	requestedDirection Direction
 	// Absolute pixel local to start drawing the sprite
-	pixelX, pixelY float64
+	currentPixelPos PixelPos
+	// The estimated pixel position of where the player will be
+	nextPixelPos PixelPos
 	// Where on the grid the play is occupying
-	tileX, tileY int
+	currentTilePos TilePos
+	// The tiles the player is about to encounter
+	nextTilePos []TilePos
 	// How fast the player moves in pixels per frame
-	moveSpeed float64
+	moveSpeed float32
 }
 
-func (p *Player) getNextPixelCoord() (float64, float64) {
-	nextX, nextY := p.pixelX, p.pixelY
-	switch p.direction {
+func (p *Player) setNextPixelCoord() {
+	switch p.currentDirection {
 	case Up:
-		nextY -= p.moveSpeed
+		p.nextPixelPos.Y = p.currentPixelPos.Y - p.moveSpeed
 	case Right:
-		nextX += p.moveSpeed
+		p.nextPixelPos.X = p.currentPixelPos.X + p.moveSpeed
 	case Down:
-		nextY += p.moveSpeed
+		p.nextPixelPos.Y = p.currentPixelPos.Y + p.moveSpeed
 	case Left:
-		nextX -= p.moveSpeed
+		p.nextPixelPos.X = p.currentPixelPos.X - p.moveSpeed
 	}
-	if nextX < 0 {
-		nextX = 0
-	} else if int(nextX) > Config.Width {
-		nextX = float64(Config.Width)
+	if p.nextPixelPos.X < 0 {
+		p.nextPixelPos.X = 0
+	} else if p.nextPixelPos.X > float32(Config.Width) {
+		p.nextPixelPos.X = float32(Config.Width)
 	}
 
-	if nextY < 0 {
-		nextY = 0
-	} else if nextY > float64(Config.Height) {
-		nextY = float64(Config.Height)
+	if p.nextPixelPos.Y < 0 {
+		p.nextPixelPos.Y = 0
+	} else if p.nextPixelPos.Y > float32(Config.Height) {
+		p.nextPixelPos.Y = float32(Config.Height)
 	}
-	return nextX, nextY
 }
 
-// func (p *Player) getCurrentTileCoord(gridSize int) (int, int) {
-// 	gridX := p.pixelX / assets.TileSize
-// 	gridY := p.pixelY / assets.TileSize
-
-// 	offsetX := int(p.pixelX) % assets.TileSize
-// 	offsetY := int(p.pixelY) % assets.TileSize
-
-// 	coverageX := float64(offsetX) / assets.TileSize
-// 	coverageY := float64(offsetY) / assets.TileSize
-
-// 	// Determine the tile that the player is mostly covering
-// 	var tileX, tileY float64
-// 	if coverageX >= 0.50 {
-// 		tileX = gridX + 1
-// 	} else {
-// 		tileX = gridX
-// 	}
-// 	if coverageY >= 0.50 {
-// 		tileY = gridY + 1
-// 	} else {
-// 		tileY = gridY
+// func (p *Player) getNextTileDiagCoord(nextX, nextY int) (nextDiagX1 int, nextDiagY1 int, nextDiagX2 int, nextDiagY2 int) {
+// 	switch p.currentDirection {
+// 	case Up:
+// 		fallthrough
+// 	case Down:
+// 		nextDiagX1 = nextX - 1
+// 		nextDiagY1 = nextY
+// 		nextDiagX2 = nextX + 1
+// 		nextDiagY2 = nextY
+// 	case Left:
+// 		fallthrough
+// 	case Right:
+// 		nextDiagX1 = nextX
+// 		nextDiagY1 = nextY - 1
+// 		nextDiagX2 = nextX
+// 		nextDiagY2 = nextY + 1
 // 	}
 
-// 	// Prevent overflows
-// 	gridSizeF := float64(gridSize)
-// 	if tileX > gridSizeF-1 {
-// 		tileX = gridSizeF - 1
-// 	} else if tileX < 0.5 {
-// 		tileX = 0
-// 	} else if tileX > 0.5 && tileX < 1 {
-// 		tileX = 1
-// 	}
-// 	if tileY > gridSizeF-1 {
-// 		tileY = gridSizeF - 1
-// 	} else if tileY < 0.5 {
-// 		tileY = 1
-// 	} else if tileY > 0.5 && tileY < 1 {
-// 		tileY = 1
-// 	}
-
-// 	return int(tileX), int(tileY)
+// 	return nextDiagX1, nextDiagY1, nextDiagX2, nextDiagY2
 // }
 
-func (p *Player) getNextTileCoord() (int, int) {
-	nextX, nextY := p.tileX, p.tileY
-	switch p.direction {
+func (p *Player) setNextTiles(bound int) {
+	var nextDiagX1, nextDiagY1, nextDiagX2, nextDiagY2 int
+	nextX, nextY := p.currentTilePos.X, p.currentTilePos.Y
+	switch p.requestedDirection {
 	case Up:
 		nextY--
+		nextDiagX1 = nextX - 1
+		nextDiagY1 = nextY
+		nextDiagX2 = nextX + 1
+		nextDiagY2 = nextY
 	case Right:
 		nextX++
+		nextDiagX1 = nextX
+		nextDiagY1 = nextY - 1
+		nextDiagX2 = nextX
+		nextDiagY2 = nextY + 1
 	case Down:
 		nextY++
+		nextDiagX1 = nextX - 1
+		nextDiagY1 = nextY
+		nextDiagX2 = nextX + 1
+		nextDiagY2 = nextY
 	case Left:
 		nextX--
+		nextDiagX1 = nextX
+		nextDiagY1 = nextY - 1
+		nextDiagX2 = nextX
+		nextDiagY2 = nextY + 1
 	}
 
-	return int(nextX), int(nextY)
+	if nextX < 0 {
+		nextX = 0
+	} else if nextX >= bound {
+		nextX = bound - 1
+	}
+	if nextY < 0 {
+		nextY = 0
+	} else if nextY >= bound {
+		nextY = bound - 1
+	}
+
+	p.nextTilePos = []TilePos{
+		{nextX, nextY},
+		{nextDiagX1, nextDiagY1},
+		{nextDiagX2, nextDiagY2},
+	}
 }
 
 func (p *Player) updateTileLocation() {
 	// To ensure we pick the right tile, use a pixel in the center of tile
-	centerPixelX := p.pixelX + assets.TileSize/2
-	centerPixelY := p.pixelY + assets.TileSize/2
+	centerPixelX := p.currentPixelPos.X + assets.TileSize/2
+	centerPixelY := p.currentPixelPos.Y + assets.TileSize/2
 
 	gridX := centerPixelX / assets.TileSize
 	gridY := centerPixelY / assets.TileSize
@@ -114,11 +130,11 @@ func (p *Player) updateTileLocation() {
 	offsetX := int(centerPixelX) % assets.TileSize
 	offsetY := int(centerPixelY) % assets.TileSize
 
-	coverageX := float64(offsetX) / assets.TileSize
-	coverageY := float64(offsetY) / assets.TileSize
+	coverageX := float32(offsetX) / assets.TileSize
+	coverageY := float32(offsetY) / assets.TileSize
 
 	// Determine the tile that the player is mostly covering
-	var tileX, tileY float64
+	var tileX, tileY float32
 	if coverageX >= 0.50 {
 		tileX = gridX + 1
 	} else {
@@ -130,6 +146,55 @@ func (p *Player) updateTileLocation() {
 		tileY = gridY
 	}
 
-	p.tileX = int(tileX)
-	p.tileY = int(tileY)
+	p.currentTilePos.X = int(tileX)
+	p.currentTilePos.Y = int(tileY)
+}
+
+func (p *Player) Update(grid *[][]assets.Tile, levelImage *ebiten.Image) {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		p.requestedDirection = Right
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		p.requestedDirection = Left
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		p.requestedDirection = Up
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		p.requestedDirection = Down
+	}
+
+	p.setNextPixelCoord()
+	p.setNextTiles(len(*grid))
+
+	nextTilePos := p.nextTilePos[0]
+	nextTile := (*grid)[nextTilePos.Y][nextTilePos.X]
+
+	if nextTile.Type == assets.TileTypeWall {
+
+		if p.requestedDirection != p.currentDirection {
+			// They are trying to turn into a wall
+			p.currentPixelPos.X, p.currentPixelPos.Y = p.nextPixelPos.X, p.nextPixelPos.Y
+
+			p.updateTileLocation()
+			return
+		}
+
+		// Perform pixel analysis to allow proper approaches to wall tiles.
+		boundingBox := image.Rect(int(p.nextPixelPos.X), int(p.nextPixelPos.Y), int(p.nextPixelPos.X+assets.SpriteSize), int(p.nextPixelPos.Y+assets.SpriteSize))
+
+		for y := boundingBox.Min.Y; y < boundingBox.Max.Y; y++ {
+			for x := boundingBox.Min.X; x < boundingBox.Max.X; x++ {
+				r, g, b, _ := levelImage.At(x, y).RGBA()
+				// Magic numbers avoid pellet color
+				if (0 < r && r < 57054) || (0 < g && g < 57054) || (0 < b && b < 65535) {
+					return
+				}
+			}
+		}
+	} else if p.requestedDirection != p.currentDirection {
+		// They are trying to turn into a non-wall, so enable Cornering logic
+		println("Cornering")
+	}
+
+	p.currentPixelPos.X, p.currentPixelPos.Y = p.nextPixelPos.X, p.nextPixelPos.Y
+	p.currentDirection = p.requestedDirection
+	p.updateTileLocation()
 }
