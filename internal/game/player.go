b4511303868d02e3b60f7cce6effeeed15767333
braheezy <michael.c.braha@gmail.com>
2023-09-15 10:33:24 -0700

    Basic cornering implementation
    She now moves cleanly around corners but there's still imperfections and it's possible to overlap walls

--- internal/game/player.go
+++ internal/game/player.go
@@ -50,27 +50,6 @@ func (p *Player) setNextPixelCoord() {
 	}
 }
 
-// func (p *Player) getNextTileDiagCoord(nextX, nextY int) (nextDiagX1 int, nextDiagY1 int, nextDiagX2 int, nextDiagY2 int) {
-// 	switch p.currentDirection {
-// 	case Up:
-// 		fallthrough
-// 	case Down:
-// 		nextDiagX1 = nextX - 1
-// 		nextDiagY1 = nextY
-// 		nextDiagX2 = nextX + 1
-// 		nextDiagY2 = nextY
-// 	case Left:
-// 		fallthrough
-// 	case Right:
-// 		nextDiagX1 = nextX
-// 		nextDiagY1 = nextY - 1
-// 		nextDiagX2 = nextX
-// 		nextDiagY2 = nextY + 1
-// 	}
-
-// 	return nextDiagX1, nextDiagY1, nextDiagX2, nextDiagY2
-// }
-
 func (p *Player) setNextTiles(bound int) {
 	var nextDiagX1, nextDiagY1, nextDiagX2, nextDiagY2 int
 	nextX, nextY := p.currentTilePos.X, p.currentTilePos.Y
@@ -113,9 +92,9 @@ func (p *Player) setNextTiles(bound int) {
 	}
 
 	p.nextTilePos = []TilePos{
-		{nextX, nextY},
-		{nextDiagX1, nextDiagY1},
-		{nextDiagX2, nextDiagY2},
+		{nextX, nextY},           // In front
+		{nextDiagX1, nextDiagY1}, // Lower
+		{nextDiagX2, nextDiagY2}, // Upper
 	}
 }
 
@@ -161,39 +140,140 @@ func (p *Player) Update(grid *[][]assets.Tile, levelImage *ebiten.Image) {
 		p.requestedDirection = Down
 	}
 
+	// Predict next position
 	p.setNextPixelCoord()
 	p.setNextTiles(len(*grid))
 
-	nextTilePos := p.nextTilePos[0]
-	nextTile := (*grid)[nextTilePos.Y][nextTilePos.X]
+	if p.requestedDirection != p.currentDirection {
+		// Player is trying to turn
+		nextTilePos := p.nextTilePos[0]
+		lowerDiagonalTilePos := p.nextTilePos[1]
+		upperDiagonalTilePos := p.nextTilePos[2]
+		nextTile := (*grid)[nextTilePos.Y][nextTilePos.X]
+		lowerDiagonalTile := (*grid)[lowerDiagonalTilePos.Y][lowerDiagonalTilePos.X]
+		upperDiagonalTile := (*grid)[upperDiagonalTilePos.Y][upperDiagonalTilePos.X]
 
-	if nextTile.Type == assets.TileTypeWall {
+		nextTileIsWall := nextTile.Type == assets.TileTypeWall
+		upperDiagonalTileIsWall := upperDiagonalTile.Type == assets.TileTypeWall
+		lowerDiagonalTileIsWall := lowerDiagonalTile.Type == assets.TileTypeWall
 
-		if p.requestedDirection != p.currentDirection {
-			// They are trying to turn into a wall
+		if nextTileIsWall {
+			// Player is trying to turn into a wall, so ignore the direction change
+			// but do update position.
 			p.currentPixelPos.X, p.currentPixelPos.Y = p.nextPixelPos.X, p.nextPixelPos.Y
 
 			p.updateTileLocation()
 			return
 		}
 
-		// Perform pixel analysis to allow proper approaches to wall tiles.
-		boundingBox := image.Rect(int(p.nextPixelPos.X), int(p.nextPixelPos.Y), int(p.nextPixelPos.X+assets.SpriteSize), int(p.nextPixelPos.Y+assets.SpriteSize))
+		preTurn, postTurn := false, false
 
-		for y := boundingBox.Min.Y; y < boundingBox.Max.Y; y++ {
-			for x := boundingBox.Min.X; x < boundingBox.Max.X; x++ {
-				r, g, b, _ := levelImage.At(x, y).RGBA()
-				// Magic numbers avoid pellet color
-				if (0 < r && r < 57054) || (0 < g && g < 57054) || (0 < b && b < 65535) {
-					return
+		// Calculate Pac-Man's position within the current tile.
+		// The fractional part represents how far Pac-Man has moved within the tile.
+		tileFractionX := float64(int(p.currentPixelPos.X)%assets.TileSize) / float64(assets.TileSize)
+		tileFractionY := float64(int(p.currentPixelPos.Y)%assets.TileSize) / float64(assets.TileSize)
+
+		// Calculate the centerline within the current tile.
+		centerlineX := 0.5 // Assuming the centerline is at the middle of the tile (0.5).
+
+		// Check if Pac-Man has crossed the centerline.
+		if (p.currentDirection == Right && tileFractionX >= centerlineX) ||
+			(p.currentDirection == Left && tileFractionX <= centerlineX) ||
+			(p.currentDirection == Up && tileFractionY <= centerlineX) ||
+			(p.currentDirection == Down && tileFractionY >= centerlineX) {
+			// Pac-Man has crossed the centerline, indicating a post-turn state.
+			postTurn = true
+		} else {
+			// Pac-Man is not in a post-turn state, so check if he's in a pre-turn state.
+			if (!upperDiagonalTileIsWall && (p.currentDirection == Up || p.currentDirection == Right)) ||
+				(!lowerDiagonalTileIsWall && (p.currentDirection == Down || p.currentDirection == Right)) {
+				// Pac-Man is in a pre-turn state.
+				preTurn = true
+			}
+		}
+
+		if preTurn {
+			println("Pre turn")
+			// Modify Pac-Man's velocity for a 45-degree angle
+			diagonalSpeed := p.moveSpeed / 1.4142 // 1.4142 is the square root of 2 (for equal diagonal speed)
+			// Update Pac-Man's position until he reaches the centerline
+			// Assuming `centerlineX` is calculated as in the previous code example.
+			if (p.currentDirection == Right && tileFractionX < centerlineX) ||
+				(p.currentDirection == Left && tileFractionX > centerlineX) ||
+				(p.currentDirection == Up && tileFractionY > centerlineX) ||
+				(p.currentDirection == Down && tileFractionY < centerlineX) {
+				// Pac-Man is in a pre-turn state, so update his position.
+				switch p.currentDirection {
+				case Right:
+					p.currentPixelPos.X += diagonalSpeed // Move diagonally up and right
+					p.currentPixelPos.Y -= diagonalSpeed
+				case Down:
+					p.currentPixelPos.X += diagonalSpeed // Move diagonally down and right
+					p.currentPixelPos.Y += diagonalSpeed
+				case Left:
+					p.currentPixelPos.X -= diagonalSpeed // Move diagonally down and left
+					p.currentPixelPos.Y += diagonalSpeed
+				case Up:
+					p.currentPixelPos.X -= diagonalSpeed // Move diagonally up and left
+					p.currentPixelPos.Y -= diagonalSpeed
+				}
+			}
+		} else if postTurn {
+			println("Post turn")
+			// Modify Pac-Man's velocity for a 45-degree angle
+			diagonalSpeed := p.moveSpeed / 1.4142 // 1.4142 is the square root of 2 (for equal diagonal speed)
+
+			// Update Pac-Man's position until he has completed the turn
+			// Assuming `centerlineX` is calculated as in the previous code example.
+			if (p.currentDirection == Right && tileFractionX >= centerlineX) ||
+				(p.currentDirection == Left && tileFractionX <= centerlineX) ||
+				(p.currentDirection == Up && tileFractionY <= centerlineX) ||
+				(p.currentDirection == Down && tileFractionY >= centerlineX) {
+				// Pac-Man is in a post-turn state, so update his position.
+				switch p.currentDirection {
+				case Right:
+					p.currentPixelPos.X += diagonalSpeed // Move diagonally up and right
+					p.currentPixelPos.Y -= diagonalSpeed
+				case Down:
+					p.currentPixelPos.X += diagonalSpeed // Move diagonally down and right
+					p.currentPixelPos.Y += diagonalSpeed
+				case Left:
+					p.currentPixelPos.X -= diagonalSpeed // Move diagonally down and left
+					p.currentPixelPos.Y += diagonalSpeed
+				case Up:
+					p.currentPixelPos.X -= diagonalSpeed // Move diagonally up and left
+					p.currentPixelPos.Y -= diagonalSpeed
+				}
+			}
+		} else {
+			p.currentPixelPos.X, p.currentPixelPos.Y = p.nextPixelPos.X, p.nextPixelPos.Y
+
+			p.updateTileLocation()
+			return
+		}
+
+	} else {
+		// Continue in the same direction
+		nextTilePos := p.nextTilePos[0]
+		nextTile := (*grid)[nextTilePos.Y][nextTilePos.X]
+		if nextTile.Type == assets.TileTypeWall {
+			// Perform pixel analysis to allow proper approaches to wall tiles.
+			boundingBox := image.Rect(int(p.nextPixelPos.X), int(p.nextPixelPos.Y), int(p.nextPixelPos.X+assets.SpriteSize), int(p.nextPixelPos.Y+assets.SpriteSize))
+
+			for y := boundingBox.Min.Y; y < boundingBox.Max.Y; y++ {
+				for x := boundingBox.Min.X; x < boundingBox.Max.X; x++ {
+					r, g, b, _ := levelImage.At(x, y).RGBA()
+					// Magic numbers avoid pellet color
+					if (0 < r && r < 57054) || (0 < g && g < 57054) || (0 < b && b < 65535) {
+						// Player is up against a wall, stop all movement.
+						return
+					}
 				}
 			}
 		}
-	} else if p.requestedDirection != p.currentDirection {
-		// They are trying to turn into a non-wall, so enable Cornering logic
-		println("Cornering")
 	}
 
+	// Update the position of the player to the next position.
 	p.currentPixelPos.X, p.currentPixelPos.Y = p.nextPixelPos.X, p.nextPixelPos.Y
 	p.currentDirection = p.requestedDirection
 	p.updateTileLocation()
